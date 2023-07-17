4290780e0640a3acb31c4ce395c40e77d9abc003
jimmy0524 <10jmin04@naver.com>
2023-07-18 04:19:35 +0900

    환경세팅 & 리액트세팅 & 네이버로그인

---
+++ src/main/java/ydsm/binari/BinariProjectApplication.java
@@ -0,0 +1,13 @@
+package ydsm.binari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinariProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinariProjectApplication.class, args);
+	}
+
+}
